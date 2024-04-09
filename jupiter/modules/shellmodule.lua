4dc5f24441e7eb647b5dcf4df468d5ea14b39d35
TheRealEli310 <33650824+TheRealEli310@users.noreply.github.com>
2024-04-09 15:13:23 +1000

    Add modules

---
+++ jupiter/modules/shellmodule.lua
@@ -0,0 +1,5 @@
+if not multishell then
+    error("Shell module requires an advanced computer")
+end
+multishell.setTitle(1,"Kernel")
+shell.openTab("shell")
--- jupiter/startup.lua
+++ jupiter/startup.lua
@@ -1,3 +1,21 @@
 function os.version()
     return "Jupiter 1.0"
 end
+shell.setPath(".:/jupiter/programs"..string.sub(shell.path(),2))
+term.clear()
+term.setCursorPos(1,1)
+ccboot.bootcomplete = true;
+if fs.exists("/jupiter/modules") then
+    _G.modules = {}
+    local evalstring = ""
+    for i,v in ipairs(fs.list("/jupiter/modules")) do
+        _G.modules[i] = function() shell.run(v) end
+        evalstring = "_G.modules["..tostring(i).."],"..evalstring
+    end 
+    evalstring = evalstring.."sleep"
+    eval("parallel.waitForAll("..evalstring..")")
+end
+-- boot is done, hang the kernel
+while true do
+    sleep(1)
+end
